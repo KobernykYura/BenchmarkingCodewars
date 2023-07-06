73fe8415c425ac80dd458a7bf6d4e46f1769016f
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-07-06 14:15:37 +0400

    add: TwoToOne kata task

---
+++ BenchMarking/Benchmarking/Benchmarks/TwoToOneBenchmark.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Benchmarking.Codewars;
+
+namespace Benchmarking.Benchmarks
+{
+    //  a = "xyaabbbccccdefww"
+    //  b = "xxxxyyyyabklmopq"
+    //  longest(a, b) -> "abcdefklmopqwxy"
+    //
+    //  a = "c"
+    //  longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+    [SimpleJob(RuntimeMoniker.Net60)]
+    public class TwoToOneBenchmark
+    {
+        [Params("xyaabbbccccdefww", "xxxxyyyyabklmopq")]
+        public string s1;
+
+        [Params("xxxxyyyyabklmopq", "abcdefghijklmnopqrstuvwxyz")]
+        public string s2;
+
+        [Benchmark(Baseline = true)]
+        public string Longest_HashSet_SortedSet_NewString()
+            => TwoToOne.Longest_HashSet_SortedSet_NewString(s1, s2);
+
+        [Benchmark]
+        public string Longest_HashSet_SortedSet_StringBuilderAggregate()
+            => TwoToOne.Longest_HashSet_SortedSet_StringBuilderAggregate(s1, s2);
+
+        [Benchmark]
+        public string Longest_HashSet_SortMethod_StringBuilderAggregate()
+            => TwoToOne.Longest_HashSet_SortMethod_StringBuilderAggregate(s1, s2);
+
+        #region KataSolutions
+
+        [Benchmark]
+        public string Longest_Distinct_OrderBy_NewString()
+            => TwoToOne.Longest_Distinct_OrderBy_NewString(s1, s2);
+
+        [Benchmark]
+        public string Longest_Distinct_OrderBy_StringConcat()
+            => TwoToOne.Longest_Distinct_OrderBy_StringConcat(s1, s2);
+
+        #endregion
+    }
+}
---
+++ BenchMarking/Benchmarking/Codewars/TwoToOne.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace Benchmarking.Codewars
+{
+    //  a = "xyaabbbccccdefww"
+    //  b = "xxxxyyyyabklmopq"
+    //  longest(a, b) -> "abcdefklmopqwxy"
+    //
+    //  a = "abcdefghijklmnopqrstuvwxyz"
+    //  longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+    public class TwoToOne
+    {
+        private static readonly StringBuilder strBuilder = new StringBuilder();
+
+        public static string Longest_HashSet_SortedSet_NewString(string s1, string s2)
+        {
+            ISet<char> uniqueValues = new HashSet<char>(s1.Union(s2));
+            ISet<char> sortedUniqueValues = new SortedSet<char>(uniqueValues);
+
+            return new string(sortedUniqueValues.ToArray());
+        }
+
+        public static string Longest_HashSet_SortedSet_StringBuilderAggregate(string s1, string s2)
+        {
+            ISet<char> uniqueValues = new HashSet<char>(s1.Union(s2));
+            ISet<char> sortedUniqueValues = new SortedSet<char>(uniqueValues);
+
+            return uniqueValues
+                .Aggregate(strBuilder.Clear(), (acc, value) => acc.Append(value))
+                .ToString();
+        }
+
+        public static string Longest_HashSet_SortMethod_StringBuilderAggregate(string s1, string s2)
+        {
+            ISet<char> uniqueValues = new HashSet<char>(s1.Union(s2));
+            List<char> sortedUniqueValues = uniqueValues.ToList();
+            
+            sortedUniqueValues.Sort((left, right) => left.CompareTo(right));
+
+            return uniqueValues
+                .Aggregate(strBuilder.Clear(), (acc, value) => acc.Append(value))
+                .ToString();
+        }
+
+        #region KataSolutionsRegion
+        
+        public static string Longest_Distinct_OrderBy_NewString(string s1, string s2)
+        {
+            return new string((s1 + s2).Distinct().OrderBy(x => x).ToArray());
+        }
+        public static string Longest_Distinct_OrderBy_StringConcat(string s1, string s2)
+        {
+            return string.Concat((s1 + s2).Distinct().OrderBy(c => c));
+        }
+
+        public static string Longest_Union_OrderBy_NewString(string s1, string s2)
+        {
+            char[] merged = s1.Union(s2)
+                .OrderBy(x => x)
+                .ToArray();
+
+            return new string(merged);
+        }
+
+        #endregion
+    }
+}
